c5b9efad9dafd7e32a0735a3c40e953b02d0e6b2
Anirudh Thakur <anirudhthakur020@gmail.com>
2026-05-31 14:33:49 +0530

    Added Render backend URL

--- frontend/config.js
+++ frontend/config.js
@@ -4,4 +4,4 @@ window.location.hostname === "127.0.0.1";
 
 const API_BASE = isLocal
 ? "http://localhost:5000/api"
-: "https://YOUR_RENDER_URL.onrender.com/api";
+: "https://movie-ai-backend-ql2a.onrender.com";
